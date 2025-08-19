68f52dead8dcd0294934363183952b9f35e4b9a5
afram12 <tanishkshrivastav23@gmail.com>
2025-08-19 11:15:15 +0530

    3 commit

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 // const Live_url =
 //   "mongodb+srv://tanishk107:tanishk107@cluster0.htndl7i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
  const connectDb = async () => {
-  return mongoose
-    .connect(process.env.Live_url)
+  return mongoose.connect(process.env.Live_URL)
     .then(() => {
       console.log("connection stabilished successfully");
     })
